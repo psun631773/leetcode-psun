41eca17eecb3b10c1ab084a604e77bb558c666c9
psun631773 <psun631773@gmail.com>
2024-04-03 10:11:05 -0400

    longestZigZag

---
+++ tree/LongestZigZag.java
@@ -0,0 +1,31 @@
+package tree;
+
+public class LongestZigZag {
+
+    public int longestZigZag(TreeNode root) {
+
+        if (root == null)
+            return 0;
+        int leftPath = helper(root.left, 1, true);
+        int rightPath = helper(root.right, 1, false);
+
+        return Math.max(leftPath, rightPath);
+    }
+
+    private int helper(TreeNode node, int currCount, boolean isLeft) {
+        if (node == null)
+            return currCount - 1;   //因为是加1了 所以要减1
+        int leftPath = 0;
+        int rightPath = 0;
+        if (isLeft) {
+            leftPath = helper(node.left, 1, true);//左侧 相同 重置长度
+            rightPath = helper(node.right, currCount + 1, false);
+        } else {
+            leftPath = helper(node.left, currCount + 1, true);
+            rightPath = helper(node.right, 1, false);//右侧 相同 重置长度
+        }
+        return Math.max(leftPath, rightPath);
+    }
+
+
+}
